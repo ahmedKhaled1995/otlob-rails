56c8673c8b94fd7f92c2c33576867ff450af4afe
ahmedKhaled1995 <bigbossak84@gmail.com>
2021-04-02 03:13:39 +0200

    Added Login with linkedin Version 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,35 @@ class User < ApplicationRecord
    :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :linkedin]
 
 
-   def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.linkedin_data"] && session["devise.linkedin_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
+  #  def self.new_with_session(params, session)
+  #   super.tap do |user|
+  #     if data = session["devise.linkedin_data"] && session["devise.linkedin_data"]["extra"]["raw_info"]
+  #       user.email = data["email"] if user.email.blank?
+  #     end
+  #   end
+  # end
 
+
+  def self.connect_to_linkedin(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => auth.info.email).first
+      if registered_user
+        return registered_user
+      else
+        #name:auth.info.first_name,
+        user = User.create(
+                            provider:auth.provider,
+                            uid:auth.uid,
+                            email:auth.info.email,
+                            password:Devise.friendly_token[0,20],
+                          )
+      end
+
+    end
+  end   
   
 
 
