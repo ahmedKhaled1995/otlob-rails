cc798a020ad85dadfe85aa6e58f4b0788bbee2c8
zak <ahmedzak19972008@gmail.com>
2021-04-16 21:25:08 -0400

    Add index function to show latest orders

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @latesOrders = Order.where(:user_id => current_user.id).last(4)
+    return @activities, @latesOrders
+  end
+  def order_details
+    render :layout => "application"
+    def new
+    end
   end
 end
