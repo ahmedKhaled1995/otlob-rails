87d0f09ed2df846024632f14c69e3e5e30f2202e
Sondos Medhat <sondos.alagmawy@gmail.com>
2021-04-18 11:03:19 +0200

    unique values friends table

--- app/javascript/packs/order/index.js
+++ app/javascript/packs/order/index.js
@@ -1,3 +1,25 @@
+//appned all rows id to array(n.b row is unique by friend id)
+currentFriends=[]
+//var uniqueItems=[] ;
+function checkrows(){
+   $('.orderfriendtable > tbody  > tr').each(function(index, tr) { 
+         currentFriends.push(tr.id)
+        
+  });
+  uniqueItems = [...new Set(currentFriends)]
+  return uniqueItems;
+}
+
+//check if current id exist in all ids array
+function checkForMatch(array, valueToMatch){
+  for(var i = 0; i < array.length; i++){
+      if(array[i] == valueToMatch)
+          return true;
+  }
+  return false;
+}
+
+
 $(document).ready(function() {
   $('#fiendsearch_form').on("click", function (e) {
       e.preventDefault();
@@ -8,15 +30,16 @@ $(document).ready(function() {
           type:"GET",
           url:"addFriendtoOrder",
           dataType:"json",
-         // data: {user_id: userid},
-         // data: {group_name: userid},
+
           data: {friend_email: frinedemail},
           success:function(result){
-          
+            friendsids=checkrows();
+            //check if current row id already exist
+                if (!checkForMatch(friendsids,result.id)) {
             content ='<tr id="'+result.id+'"><td>'+result.id+
             '</td><td>'+result.full_name+'</td><td>'+result.email+'</td><td><button class="btn btn-danger" id="removefriend'+result.id+'">remove</button><td></tr>' ;
             $('#orderfriends').last().append(content);
-          
+                }
           }
         });
         
@@ -35,15 +58,27 @@ $(document).ready(function() {
             dataType:"json",
            data: {group_name: groupname},
             success:function(result){
+              
+             
 
             //  ---------------group-----------------
              result=result.friends
             for(i=0;i<result.length;i++){
-                content ='<tr id="'+result[i].id+'"><td>'+result[i].id+
-                '</td><td>'+result[i].full_name+'</td><td>'
-                +result[i].email+'</td><td><button class="btn btn-danger" id="removefriend'
-                +result[i].id+'">remove</button><td></tr>' ;
-                $('#orderfriends').last().append(content);
+
+                friendsids=checkrows();
+              //check if current row id already exist
+                  if (!checkForMatch(friendsids,result[i].id)) {
+                    content ='<tr id="'+result[i].id+'"><td>'+result[i].id+
+                    '</td><td>'+result[i].full_name+'</td><td>'
+                    +result[i].email+'</td><td><button class="btn btn-danger" id="removefriend'
+                    +result[i].id+'">remove</button><td></tr>' ;
+                    $('#orderfriends').last().append(content);
+                }
+
+
+
+           
+                
             }
             
             }
@@ -51,4 +86,5 @@ $(document).ready(function() {
           
     });
   });
+
 
