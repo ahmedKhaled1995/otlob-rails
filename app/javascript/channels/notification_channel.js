c42994afce6ba80ed3d9e871e07ccad9cdc13a48
ahmedKhaled1995 <bigbossak84@gmail.com>
2021-04-20 23:42:49 +0200

    Fixed conglecting channels

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -10,16 +10,16 @@ document.addEventListener('turbolinks:load', ()=>{
   const idDivElement = document.querySelector("#user-id");
   if (idDivElement){
     const user_id = idDivElement.getAttribute("data-user-id");
-    
     // We check to see of connection exists, if so we don't create a new connection
     const subscriptions = consumer.subscriptions.subscriptions;
-    let notificationsubscription = subscriptions.find((subscriptionsObject)=>{
+    let notificationChannel = subscriptions.find((subscriptionsObject)=>{
       const identifierObj = JSON.parse(subscriptionsObject.identifier);
-      return identifierObj.channel = "NotificationChannel"
+      return identifierObj.channel === "NotificationChannel"
     });
-
+    
     // Establishing a connection channedl if a connection hasn't already been established
-    if(!notificationsubscription){
+    if(!notificationChannel){
+  
       consumer.subscriptions.create({ channel: "NotificationChannel", user_id: user_id }, {
         connected() {
           // Called when the subscription is ready for use on the server
