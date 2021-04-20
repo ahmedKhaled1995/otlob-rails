da3b51e5416e6d84772038a6e9b757fcc7ca1448
Sondos Medhat <sondos.alagmawy@gmail.com>
2021-04-20 23:58:10 +0200

    edit cached friends

--- app/javascript/packs/order/index.js
+++ app/javascript/packs/order/index.js
@@ -10,13 +10,15 @@ function checkrows(){
   uniqueItems = [...new Set(currentFriends)]
 
 
-  numberoffriends=(uniqueItems.length)
+  nofriends=(uniqueItems.length)
   // console.log(uniqueItems)
-  // console.log(numberoffriends)
-  $('#numberoffriends').val(numberoffriends);
+  // console.log(nofriends)
+  $('#numberoffriends').val(nofriends);
 
   return uniqueItems;
 }
+
+
 
 //check if current id exist in all ids array
 function checkForMatch(array, valueToMatch){
@@ -42,32 +44,31 @@ $(document).ready(function() {
           data: {friend_email: frinedemail},
           success:function(res){
             friendsids=checkrows();
-
-
-          
-            
+       
             //check if current row id already exist
             //res[1] -> user object , res[0] -> friend id
             result=res[1]
             var imageUrl;
                 if (!checkForMatch(friendsids,res[0])) {
                 //mfrod hna y7sl error :) res[0] id just an id
-                  if(res[0].get_image == null)
+              
+                if(res[0].id.get_image == null)
                   {
+                    
                      imageUrl= `https://gravatar.com/avatar/${CryptoJS.MD5(result.email).toString()}?s=32&d=identicon&r=PG`
                     //  console.log(imageUrl,)
 
                   }
                   else{
-                    imageUrl=res.get_image
+                    imageUrl=res[0].get_image
                     // console.log(res.get_image)
                   }
                   
-                  content ='<tr class="card" id="'+res[0]+'" style="margin-bottom: 20px;">'+
+                  content ='<tr class="card" id="'+res[0].id+'" style="margin-bottom: 20px;">'+
                   '<td class="card-header"><img src="'+imageUrl+'" alt="profile_picture" width="50" height="50">'
-                  +res[0]+' : '+result.full_name+'</td><td>'+
-                  result.email+'</td><td class="card-footer"><button class="btn btn-danger" id="removefriend'+
-                  res[0]+'" style="margin-left: 70%;">remove</button></td></tr>' ;
+                  +res[0].id+' : '+result.full_name+'</td><td>'+
+                  result.email+'</td><td class="card-footer"><button class="btn btn-danger removefriend" id="removefriend'+
+                  res[0].id+'" style="margin-left: 70%;">remove</button></td></tr>' ;
                   $('#orderfriends').last().append(content);
                 }
 
@@ -99,9 +100,6 @@ $(document).ready(function() {
             //  console.log(res[0].friendsid[i])
                 friendsids=checkrows();
 
-            
-
-
               //check if current row id already exist
                   if (!checkForMatch(friendsids,res[0].friendsid[i])) {
 
@@ -120,7 +118,7 @@ $(document).ready(function() {
 
                     content ='<tr class="card" id="'+res[0].friendsid[i]+'" style="margin-bottom:20px">'+
                     '<td class="card-header">'+'<img src="'+imageUrl+'" alt="profile_picture" width="50" height="50">'+res[0].friendsid[i]+' : '+result[i].full_name+'</td><td class="card-body">'
-                    +result[i].email+'</td><td class="card-footer"><button style="margin-left:70%"class="btn btn-danger" id="removefriend'
+                    +result[i].email+'</td><td class="card-footer"><button style="margin-left:70%"class="btn btn-danger removefriend" id="removefriend'
                     +res[0].friendsid[i]+'">remove</button></td></tr>' ;
                     $('#orderfriends').last().append(content);
 
@@ -137,6 +135,7 @@ $(document).ready(function() {
 
  function allIds(){
         formData=checkrows();
+        //formData=uniqueItems;
         // console.log('thisis',formData)
             $.ajax({
               type: 'GET',
@@ -146,3 +145,47 @@ $(document).ready(function() {
         
           })
 }
+// $('#submit-order').on("click", function (e) {
+//   currentFriends=[];
+//   uniqueItems=[];uniqueItems
+
+// });
+
+
+// function deleterows(idToRemove){
+//   for(var k = 0; k < uniqueItems.length; k++){
+//     if(uniqueItems[k] == idToRemove)
+//    {  
+//     uniqueItems.splice(k,1);
+//     }
+//     }
+
+//     for(var l = 0; l< currentFriends.length; l++){
+//       if(currentFriends[l] == idToRemove)
+//      {   
+//       currentFriends.splice(l,1);
+//       }
+//       }
+
+//   // oldfriendconut=  $('#numberoffriends').val();
+//   // newfriendconut=oldfriendconut-1;
+//   // $('#numberoffriends').val(newfriendconut)
+
+
+//   nooffriends=(uniqueItems.length)
+//   // console.log(uniqueItems)
+//   // console.log(nooffriends)
+  
+//   $('#numberoffriends').val(nooffriends);
+
+//   return currentFriends;
+// }
+
+// $(document).on("click",'.removefriend',function(){
+//   // console.log(currentFriends)
+//   idvalue =$(this).closest('tr').attr('id')
+//  // console.log(idvalue)
+//   $(this).closest('tr').remove(); 
+//   deleterows(idvalue);
+//   // allIds()
+// });
