46d574777829e6fed63f58049d9ec7de91dda493
ahmedKhaled1995 <bigbossak84@gmail.com>
2021-04-22 01:28:31 +0200

    Solved Schema conflict

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,4 @@
 class Restaurant < ApplicationRecord
     validates :name, presence: true
-<<<<<<< HEAD
-    
-=======
     has_one_attached :menu
->>>>>>> d305d2cccdea16aca847a421eb110537a24e074b
 end
