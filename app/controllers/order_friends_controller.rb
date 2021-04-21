cc8da7ab8b2bcf033fc732ebf4ae71747ca140f3
Sondos Medhat <sondos.alagmawy@gmail.com>
2021-04-21 09:17:30 +0200

    view joined and invited friends after added and delete

--- app/controllers/order_friends_controller.rb
+++ app/controllers/order_friends_controller.rb
@@ -1,5 +1,29 @@
 class OrderFriendsController < ApplicationController
   def index
+    if(params[:status] == "0")
+    
+      @orders_friends = OrderFriend.where(order:params[:order_id],status:0)
+      @invitbit="0"
+      @order=params[:order_id]
+    
+    else
+      @orders_friends = OrderFriend.where(order:params[:order_id],status:1)
+      @invitbit="1"
+      @order=params[:order_id]
+    end
   end
 
+  def show
+  end
+
+  def destroy
+    @order_friend = OrderFriend.find(params[:id])
+    @order_friend.destroy
+     if params[:status] == "0"
+       redirect_to order_order_friends_path(params[:order_id],status:0), notice: "Friend deleted successfully from invited friends .."
+     else
+      redirect_to order_order_friends_path(params[:order_id],status:1), notice: "Friend deleted successfully from joined friend .."
+
+     end
+  end
 end
