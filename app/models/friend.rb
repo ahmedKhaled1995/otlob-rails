dccca0486de19c3e5619a05d5b11c1f8f091a613
ahmedKhaled1995 <bigbossak84@gmail.com>
2021-04-23 12:44:27 +0200

    Fixed invited orders count

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -5,4 +5,20 @@ class Friend < ApplicationRecord
 
 
   validates :email, presence: true
+
+  # before_save :check_friend
+
+  # private
+  #   def check_friend(friend_record)
+  #     friend_exists = false
+  #     current_user.friends.each do |user_friend|
+  #       if user_friend.email == friend_record.email
+  #         friend_exists = true
+  #       end
+  #     end
+  #     if friend_exists || friend_record.email == current_user.email
+  #       errors.add(:email, "already exists")
+  #     end
+  #   end
+
 end
